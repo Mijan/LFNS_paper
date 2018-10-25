5ee9887719736ab1dd5d1b933f8d1f5f1dc45763
jan <jan.mikelson@bsse.ethz.ch>
2018-10-25 15:07:11 +0200

    test_lfns updated

--- test_lfns.cpp
+++ test_lfns.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "src/LFNS/LFNSSettings.h"
 #include "src/LFNS/LFNS.h"
+#include "src/LFNS/seq/LFNSSeq.h"
 
 
 double log_likelihood_function(const std::vector<double> &param) {
@@ -9,8 +10,7 @@ double log_likelihood_function(const std::vector<double> &param) {
 
 int main() {
     lfns::LFNSSettings settings;
-    settings.parameter_log_scale = {false};
-    settings.parameter_bounds = {std::make_pair(0, 1)};
+    settings.parameters.push_back(lfns::ParameterSetting("param_1", {0, 1}, false, false));
     settings.uniform_prior = true;
     settings.estimator = lfns::REJECT_DPGMM;
     settings.output_file = "/home/jan/crypt/Dropbox/Nested_Sampling_paper/numerical_data/tmp/lfns_output.txt";
@@ -18,8 +18,10 @@ int main() {
     settings.N = 1000;
     settings.r = 100;
 
+    base::RngPtr rng = std::make_shared<base::RandomNumberGenerator>(time(NULL));
     std::function<double(const std::vector<double>)> fct = log_likelihood_function;
-    lfns::LFNS lfns(settings, fct);
+    lfns::LogLikelihodEvalFct_ptr fct_ptr = std::make_shared<lfns::LogLikelihodEvalFct>(fct);
+    lfns::seq::LFNSSeq lfns(settings, rng, fct_ptr);
     lfns.runLFNS();
     return 1;
 }
