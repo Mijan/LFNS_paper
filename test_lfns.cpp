7a1905da94e2c1f281d9bb25fb1f4af75930e0b3
jan <jan.mikelson@bsse.ethz.ch>
2018-10-28 23:09:29 +0100

    tests removed

--- test_lfns.cpp
+++ test_lfns.cpp
@@ -9,19 +9,19 @@ double log_likelihood_function(const std::vector<double> &param) {
 }
 
 int main() {
-    lfns::LFNSSettings settings;
-    settings.parameters.push_back(lfns::ParameterSetting("param_1", {0, 1}, false, false));
-    settings.uniform_prior = true;
-    settings.estimator = lfns::REJECT_DPGMM;
-    settings.output_file = "/home/jan/crypt/Dropbox/Nested_Sampling_paper/numerical_data/tmp/lfns_output.txt";
-    settings.log_termination = -4.6;
-    settings.N = 1000;
-    settings.r = 100;
-
-    base::RngPtr rng = std::make_shared<base::RandomNumberGenerator>(time(NULL));
-    std::function<double(const std::vector<double>)> fct = log_likelihood_function;
-    lfns::LogLikelihodEvalFct_ptr fct_ptr = std::make_shared<lfns::LogLikelihodEvalFct>(fct);
-    lfns::seq::LFNSSeq lfns(settings, rng, fct_ptr);
-    lfns.runLFNS();
-    return 1;
+//    lfns::LFNSSettings settings;
+//    settings.parameters.push_back(lfns::ParameterSetting("param_1", {0, 1}, false, false));
+//    settings.uniform_prior = true;
+//    settings.estimator = lfns::REJECT_DPGMM;
+//    settings.output_file = "/home/jan/crypt/Dropbox/Nested_Sampling_paper/numerical_data/tmp/lfns_output.txt";
+//    settings.log_termination = -4.6;
+//    settings.N = 1000;
+//    settings.r = 100;
+//
+//    base::RngPtr rng = std::make_shared<base::RandomNumberGenerator>(time(NULL));
+//    std::function<double(const std::vector<double>)> fct = log_likelihood_function;
+//    lfns::LogLikelihodEvalFct_ptr fct_ptr = std::make_shared<lfns::LogLikelihodEvalFct>(fct);
+//    lfns::seq::LFNSSeq lfns(settings, rng, fct_ptr);
+//    lfns.runLFNS();
+//    return 1;
 }
--- test_models.cpp
+++ test_models.cpp
@@ -13,26 +13,26 @@
 
 
 int testReadModel() {
-    std::string model_file = "/home/jan/crypt/Dropbox/Nested_Sampling_paper/numerical_data/tmp/lacgfp_model.txt";
-    models::ModelReactionData model_data(model_file);
-    models::ChemicalReactionNetwork lac_system(model_data);
-
-    lac_system.printInfo(std::cout);
-
-
-    base::RngPtr rng = std::make_shared<base::RandomNumberGenerator>(time(NULL));
-    std::string initial_value_file = "/home/jan/crypt/Dropbox/Nested_Sampling_paper/numerical_data/tmp/lacgfp_initial_states.txt";
-    models::InitialValueData init_data(initial_value_file);
-    models::InitialValueProvider lac_init_value(rng, init_data);
-
-    lac_init_value.printInfo(std::cout);
-
-
-    std::string measurement_file = "/home/jan/crypt/Dropbox/Nested_Sampling_paper/numerical_data/tmp/lacgfp_measurement.txt";
-    models::MeasurementModelData measure_data(measurement_file);
-    models::MeasurementModel lac_measure(rng, measure_data);
-
-    lac_measure.printInfo(std::cout);
+//    std::string model_file = "/home/jan/crypt/Dropbox/Nested_Sampling_paper/numerical_data/tmp/lacgfp_model.txt";
+//    models::ModelReactionData model_data(model_file);
+//    models::ChemicalReactionNetwork lac_system(model_data);
+//
+//    lac_system.printInfo(std::cout);
+//
+//
+//    base::RngPtr rng = std::make_shared<base::RandomNumberGenerator>(time(NULL));
+//    std::string initial_value_file = "/home/jan/crypt/Dropbox/Nested_Sampling_paper/numerical_data/tmp/lacgfp_initial_states.txt";
+//    models::InitialValueData init_data(initial_value_file);
+//    models::InitialValueProvider lac_init_value(rng, init_data);
+//
+//    lac_init_value.printInfo(std::cout);
+//
+//
+//    std::string measurement_file = "/home/jan/crypt/Dropbox/Nested_Sampling_paper/numerical_data/tmp/lacgfp_measurement.txt";
+//    models::MeasurementModelData measure_data(measurement_file);
+//    models::MeasurementModel lac_measure(rng, measure_data);
+//
+//    lac_measure.printInfo(std::cout);
     return 1;
 }
 
